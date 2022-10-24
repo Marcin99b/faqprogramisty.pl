3d5d09e9958655471a1d9c83773875c805a22ac2
Marcin Bigos <marcin.bigos99@gmail.com>
2022-10-24 04:07:22 +0200

    add clarity and update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,20 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-clarity`,
+      options: {
+        // String value for your clarity project id
+        // Project id is found in your clarity dashboard url
+        // https://clarity.microsoft.com/projects/view/{clarity_project_id}/
+        clarity_project_id: 'e7t37l92kd',
+        // Boolean value for enabling clarity while developing
+        // true will enable clarity tracking code on both development and production environments
+        // false will enable clarity tracking code on production environment only
+        //
+        enable_on_dev_env: true
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
